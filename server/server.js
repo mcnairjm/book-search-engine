74d37d79b2248aad9287d4961d10e8cd58da9403
mcnairjm19 <mcnairjm1993@gmail.com>
2022-04-13 10:04:57 -0400

    finished with back end

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth')
 
+const { typeDefs, resolvers } = require('./schemas')
+
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+/* const routes = require('./routes'); */
 
 const app = express();
 const PORT = process.env.PORT || 3001;
